7f9443a6bce8efff2283cd2c41bd1cd80f0f1317
pTpTk <zxu44@u.rochester.edu>
2026-03-02 11:51:55 +0000

    correct bulk cp

--- HBM_bw_single_thread_bulk/main.cu
+++ HBM_bw_single_thread_bulk/main.cu
@@ -6,6 +6,12 @@
 #include <curand_kernel.h>
 #include <time.h>
 #include <nvml.h>
+#include <cuda/ptx>
+#include <cuda/barrier>
+
+#define DATA_SIZE 200000
+namespace ptx = cuda::ptx;
+using barrier = cuda::barrier<cuda::thread_scope_block>;
 
 __global__ void kernel(unsigned int* d_data,
                          unsigned int freq,
@@ -15,19 +21,35 @@ __global__ void kernel(unsigned int* d_data,
     volatile unsigned long long accumulator = 0;
     volatile unsigned long long value = 0;
 
+    #pragma nv_diag_suppress static_var_with_dynamic_init
+    __shared__ barrier bar;
+    if (threadIdx.x == 0) {
+        init(&bar, blockDim.x);
+    }
     __syncthreads();
-    size_t smem_addr = static_cast<size_t>(__cvta_generic_to_shared(smem));
+
+    uint32_t smem_addr = static_cast<uint32_t>(__cvta_generic_to_shared(smem));
     size_t gmem_addr = static_cast<size_t>(__cvta_generic_to_global(d_data));
 
     auto start = clock64();
-    asm volatile(
-        "cp.async.bulk.shared.global [%0], [%1], %2;\n" 
-        : 
-        : "l"(smem_addr), "l"(gmem_addr), "n"(232448) 
-    );
 
-    asm volatile("cp.async.bulk.commit_group;\n" ::);
-    asm volatile("cp.async.bulk.wait_group 0;\n" ::);
+    asm volatile(
+      "cp.async.bulk.shared::cta.global.mbarrier::complete_tx::bytes [%0], [%1], %2, [%3];\n"
+        :
+        : "l"(__cvta_generic_to_shared(smem)), "l"(reinterpret_cast<uint64_t>(d_data)), "n"(232448),
+            "l"(__cvta_generic_to_shared(&bar))
+        : "memory");
+
+    auto __bh = __cvta_generic_to_shared(cuda::device::barrier_native_handle(bar));
+    asm("mbarrier.expect_tx.relaxed.cta.shared::cta.b64 [%0], %1;"
+        :
+        : "r"(static_cast<_CUDA_VSTD::uint32_t>(__bh)),
+          "n"(232448)
+        : "memory");
+
+    barrier::arrival_token token = bar.arrive();
+    bar.wait(std::move(token));
+
     auto end = clock64();
     // Main measurement loop.
 #pragma unroll 1
